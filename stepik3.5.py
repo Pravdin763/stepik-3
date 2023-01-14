f3dd3b763ab5ef3cbc374d6eb53a3d21d13203a1
igor <igor.pravdin163@gmail.com>
2023-01-14 23:30:35 +0400

    stepik 3.5.7 с коментами

--- stepik3.5.py
+++ stepik3.5.py
@@ -6,28 +6,27 @@ x = [input() for i in range(int(input()))]    # список имен и дат 
 res = []            # список куда будут входить даты подходящие под условие! дата(num) < дат рожд <= дата(num+7)
 
 def exemyear(d, n, r): # d=день рожд, n=дата, r=res функция проверки условий дата(num) < дат рожд <= дата(num+7)
-    d1 = d-timedelta(days=0)        # тажа дата, превращается в нужный формат для вывода month
-    d2 = d-timedelta(days=7)
-    if d1.month<d2.month:
-        n1 = n-timedelta(days=365)
-        n2 = n+timedelta(days=365)
-        if n1 < d1 <=n2:
-            r.append(d1)
+    d2 = d-timedelta(days=7)    # d2 = дата рожд - 7 дней
+    if d.month<d2.month:    # если месяц даты рожд < меньше месяца даты рожд -7дней. Например (5.01.20< 29.12.20)
+        n1 = n-timedelta(days=365)    # дату уменьшаем на год
+        n2 = n+timedelta(days=365)    # дату увеличиваем на год
+        if n1 < d <=n2:    # если дата рожд в промежутке
+            r.append(d)    # добавляем в res
         return r
-    else:
+    else:                # иначе, оставляем как было не уменьшая и не увеличивая год. т.к он не меняется от 7 дней
         if n < d <= n + timedelta(days=7):
-            r.append(d1)
+            r.append(d)    # тоже добавляем
             return r
 
-for i in x:
-    i2 = i.split()[2].split('.')
-    d = date(day=int(i2[0]), month=int(i2[1]), year=int(i2[2]))
-    n = date(day=int(num[0]), month=int(num[1]), year=int(i2[2]))
-    exemyear(d, n, res)
+for i in x:        # итерация по датам с именами
+    i2 = i.split()[2].split('.')    # 2 раза разбиваем, чтобы получить только дату
+    d = date(day=int(i2[0]), month=int(i2[1]), year=int(i2[2]))    # день рожд!
+    n = date(day=int(num[0]), month=int(num[1]), year=int(i2[2]))    # дата , НО год как у именинника (меняется)
+    exemyear(d, n, res)        # вызов функции для проверки n<d<n+7
 
-if len(res) > 0:
-    for i in x:
-        if max(res).strftime('%d.%m.%Y') in i:
-            print(*i.split()[:2])
+if len(res) > 0:    # если список не пуст
+    for i in x:     # итерируемся по основному списку, чтобы вывести имя
+        if max(res).strftime('%d.%m.%Y') in i:   # макс. дата из списка(самый молодой) переводим в str, в строках
+            print(*i.split()[:2])    # выводим имя совпадающее с макс датой из res
 else:
     print('Дни рождения не планируются')
